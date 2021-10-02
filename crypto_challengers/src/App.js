09591525ae6479eb5dd595b4ed820c88d6366b08
Alfred Jophy <alfredjophy@protonmail.com>
2021-10-02 19:22:30 +0530

    basic

--- crypto_challengers/src/App.js
+++ crypto_challengers/src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./navbar";
+import Section1 from "./section_1";
 
 function App() {
-    return (
-        <div>
-        <h1>
-        Hello world
-        </h1>
-        <h2>
-        bye bye world
-        </h2>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Section1 />
+    </div>
+  );
 }
 
 export default App;
---
+++ crypto_challengers/src/navbar.js
@@ -0,0 +1,11 @@
+import style from "./navbar.module.css";
+
+function Navbar() {
+  return (
+    <div className={style.navbar}>
+      <h1>Navbar</h1>
+    </div>
+  );
+}
+
+export default Navbar;
