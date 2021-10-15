fbc451a1e9638dfebca1ac06563d8f4d93c88fba
Alfred Jophy <alfredjophy@protonmail.com>
2021-10-15 18:23:54 +0530

    got ama card

--- crypto_challengers/src/App.js
+++ crypto_challengers/src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
-import Navbar from "./navbar";
-import Section1 from "./section_1";
+//import Navbar from "./navbar";
+//import Section1 from "./section_1";
 import AMA from "./ama";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Section1 />
-      <AMA />
-    </div>
-  );
+    return (
+        <div>
+            <AMA />
+        </div>
+    );
 }
 
 export default App;
--- crypto_challengers/src/ama.js
+++ crypto_challengers/src/ama.js
@@ -1,45 +1,122 @@
 import { useState, useEffect } from "react";
 import "./ama.css";
+import logo from "./assets/logo-white.png";
 
-//https://medium.com/feed/@CryptoChallengersD
+const Header = () => {
+    return (
+        <header>
+            <nav
+                className="
+          navbar navbar-expand-lg navbar-light
+          fixed-top
+          bg-light
+          text-uppercase
+        "
+            >
+                <a classNam="navbar-brand" href="index.html">
+                    <img
+                        src={logo}
+                        alt=""
+                        width="50"
+                        className="d-inline-block align-text-top"
+                    />
+                </a>
+            </nav>
+        </header>
+    );
+};
 
-function Card(props) {
-  return (
-    <a href={props.post.link}>
-      <img src={props.post.thumbnail} height="100px" width="100px" />
-      <h4>{props.post.title}</h4>
-    </a>
-  );
-}
+const RecentCard = (props) => {
+    return (
+        <section>
+            <a href={props.post.link}>
+                <div className="container">
+                    <div className="latest mt-5">
+                        <div className="row">
+                            <div className="image col-6">
+                                <img
+                                    className="img-fluid"
+                                    src={props.post.thumbnail}
+                                    alt=""
+                                    loading="lazy"
+                                />
+                            </div>
+                            <div className="content col-6">
+                                <h2>{props.post.title}</h2>
+                                <h3>{props.post.pubDate}</h3>
+                                <p>Lorem Ipsum</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </section>
+    );
+};
 
-function AMA() {
-  const [posts, setPosts] = useState(null);
-  useEffect(() => {
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ title: "React Hooks POST Request Example" }),
-    };
+const Card = (props) => {
+    return (
+        <a href={props.post.link}>
+            <div className="card older">
+                <div className="old-img">
+                    <img
+                        className="img-fluid"
+                        src={props.post.thumbnail}
+                        alt=""
+                    />
+                </div>
 
-    getData();
+                <div className="old-content">
+                    <h2>{props.post.title}</h2>
+                    <h3>{props.post.pubDate}</h3>
+                    <p>Lorem Ipsum</p>
+                </div>
+            </div>
+        </a>
+    );
+};
 
-    async function getData() {
-      const response = await fetch(
-        "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@CryptoChallengersD",
-        requestOptions
-      );
-      const data = await response.json();
-      console.log(data);
-      setPosts(data.items);
-    }
-  }, []);
+const Cards = (props) => {
+    return (
+        <section>
+            <div class="cards">
+                {props.post.map((post, index) => (
+                    <Card post={post} key={index} />
+                ))}
+            </div>
+        </section>
+    );
+};
 
-  return (
-    <div>
-      <h1>AMA</h1>
-      {posts && posts.map((post, index) => <Card post={post} key={index} />)}
-    </div>
-  );
-}
+const AMA = () => {
+    const [posts, setPosts] = useState(null);
+    useEffect(() => {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ title: "Getting AMAs from medium" }),
+        };
+
+        async function getData() {
+            const response = await fetch(
+                "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@CryptoChallengersD",
+                requestOptions
+            );
+            const data = await response.json();
+            setPosts(data.items);
+            console.log(posts);
+        }
+
+        getData();
+    }, []);
+
+    if (posts == null) return null;
+    return (
+        <div>
+            <Header />
+            <RecentCard post={posts[2]} />
+        </div>
+    );
+};
 
 export default AMA;
