e114f1ac86b3fdf712ea4be20ce8a1c815e0e45a
Alfred Jophy <alfredjophy@protonmail.com>
2022-03-21 05:38:01 +0530

    minor fixes

--- js/index.js
+++ js/index.js
@@ -18,8 +18,116 @@ HTMLElement.prototype.serialize = function () {
     return res;
 };
 
+function getAds() {
+    const data = [
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "11",
+            filename: "11647704358.jpg",
+            id: 57,
+            title: "1",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "",
+            filename: "1647711942.",
+            id: 67,
+            title: "",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description:
+                ".form {         padding: 0;         width: 90%;         padding-left: 15%;     }",
+            filename: "kmlklkdfs1647711628.jpg",
+            id: 66,
+            title: "kmlklkdfs",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "",
+            filename: "1647711611.",
+            id: 65,
+            title: "",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "kajsldkjalskjdlaksjdlkja",
+            filename: "hj1647710915.jpg",
+            id: 64,
+            title: "hj",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "",
+            filename: "1647710848.",
+            id: 63,
+            title: "",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "55",
+            filename: "551647708280.jpg",
+            id: 62,
+            title: "55",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "",
+            filename: "1647708106.jpg",
+            id: 61,
+            title: "",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "44",
+            filename: "41647704572.jpg",
+            id: 60,
+            title: "4",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "33",
+            filename: "31647704557.jpg",
+            id: 59,
+            title: "3",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "22",
+            filename: "21647704538.jpg",
+            id: 58,
+            title: "2",
+        },
+        {
+            date: "Sat, 19 Mar 2022 00:00:00 GMT",
+            description: "",
+            filename: "1647712058.",
+            id: 68,
+            title: "",
+        },
+    ];
+    return data;
+}
+function renderAds() {
+    const adb = document.querySelector(".swiper-wrapper");
+    const ads = getAds();
+    const innerHTML = ads.reduce((prev, cur) => {
+        return (
+            prev +
+            `
+            <div class="swiper-slide adbox" >
+            <img src="http://localhost:5000/ad/${cur.filename}" alt="">
+            <h1>${cur.title}</h1>
+            <h3>${cur.description}</h3>
+            </div>
+            `
+        );
+    }, "");
+    adb.innerHTML = innerHTML;
+}
+
 ready(() => {
-    var typed = new Typed("#typed", {
+    const typed = new Typed("#typed", {
         strings: ["#WeAreChallengers"],
         smartBackspace: true, // Default value
         startDelay: 10,
@@ -29,7 +137,7 @@ ready(() => {
         loop: true,
     });
     //animation
-    var wow = new WOW({
+    const wow = new WOW({
         boxClass: "wow", // animated element css class (default is wow)
         animateClass: "animated", // animation css class (default is animated)
         offset: 0, // distance to the element when triggering the animation (default is 0)
@@ -43,45 +151,20 @@ ready(() => {
         resetAnimation: true, // reset animation on end (default is true)
     });
     wow.init();
-    
-    
-    
-    function getAds()
-    {
-      const data =[{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"11","filename":"11647704358.jpg","id":57,"title":"1"},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"","filename":"1647711942.","id":67,"title":""},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":".form {         padding: 0;         width: 90%;         padding-left: 15%;     }","filename":"kmlklkdfs1647711628.jpg","id":66,"title":"kmlklkdfs"},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"","filename":"1647711611.","id":65,"title":""},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"kajsldkjalskjdlaksjdlkja","filename":"hj1647710915.jpg","id":64,"title":"hj"},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"","filename":"1647710848.","id":63,"title":""},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"55","filename":"551647708280.jpg","id":62,"title":"55"},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"","filename":"1647708106.jpg","id":61,"title":""},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"44","filename":"41647704572.jpg","id":60,"title":"4"},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"33","filename":"31647704557.jpg","id":59,"title":"3"},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"22","filename":"21647704538.jpg","id":58,"title":"2"},{"date":"Sat, 19 Mar 2022 00:00:00 GMT","description":"","filename":"1647712058.","id":68,"title":""}]
-      return data;
-    }
-    function renderAds(){
-        const adb = document.querySelector(".swiper-wrapper");
-        const ads=getAds();
-        const innerHTML=ads.reduce((prev,cur)=>{
-            return prev+`
-            <div class="swiper-slide adbox" >
-            <img src="http://localhost:5000/ad/${cur.filename}" alt="">
-            <h1>${cur.title}</h1>
-            <h3>${cur.description}</h3>
-            </div>
-            `
-        },"");
-        adb.innerHTML=innerHTML;
-    }
-    var swiper = new Swiper(".mySwiperad", {
+
+    const swiperAd = new Swiper(".mySwiperad", {
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
-          },
+        },
         autoplay: {
             delay: 2500,
-          },
+        },
     });
 
-    
     renderAds();
-    
-    
 
-
-    var swiper = new Swiper(".mySwiper", {
+    const swiper = new Swiper(".mySwiper", {
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
